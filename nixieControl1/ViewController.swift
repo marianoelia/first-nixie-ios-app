6157877a2e3c730169cd97a0d7329b798fd390e7
Mariano Elia <mrelia100@gmail.com>
2017-04-06 13:06:13 -0400

    Reading variable

--- nixieControl1/ViewController.swift
+++ nixieControl1/ViewController.swift
@@ -7,8 +7,12 @@
 //
 
 import UIKit
+import Spark_SDK
+import SparkSetup
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var startDeviceSetup: NSLayoutConstraint!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +24,39 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    
+    @IBAction func startDeviceSetup(_ sender: Any) {
+        
+        let setupController = SparkSetupMainController()
+        self.present(setupController!, animated: true, completion: nil)
+        
+    }
+    
+    
+    @IBAction func readVariableButtonTapped(_ sender: Any) {
+        SparkCloud.sharedInstance().getDevices { (sparkDevicesList : [Any]?, error :Error?) -> Void in
+            // 2
+            if let sparkDevices = sparkDevicesList as? [SparkDevice]
+            {
+                print(sparkDevices)
+                // 3
+                for device in sparkDevices
+                {
+                    if device.name == "myDevice"
+                    {
+                        // 4
+                        device.callFunction("digitalwrite", withArguments: ["D6","HIGH"], completion: { (resultCode :NSNumber?, error : Error?) -> Void in
+                            // 5
+                           print("Called a function on myDevice")
+                        })
+                    }
+                }
+            }
+        }
+    }
+    
+    
 }
+
+
 
